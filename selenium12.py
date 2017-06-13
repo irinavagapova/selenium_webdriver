73adbf3075a3af0df63003b6be550a0dc47ade60
irinavagapova <irina.vagapova@gmail.com>
2017-06-14 00:05:08 +0300

    sel12

--- selenium12.py
+++ selenium12.py
@@ -1,5 +1,8 @@
 import pytest
 from selenium import webdriver
+import urllib.request
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 @pytest.fixture
 def driver(request):
@@ -17,5 +20,24 @@ def test_example(driver):
     driver.find_element_by_link_text("Add New Product").click()
     driver.implicitly_wait(10)
     driver.find_element_by_css_selector("input[type='radio'][value='1']").click()
-    driver.find_element_by_name("name[en]").send_keys("Flower")
-    print('ggg')
+    driver.find_element_by_name("name[en]").send_keys("Cat")
+
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/irinavagapova/selenium_webdriver/master/cat.jpg", "D:\\cat.jpg")
+    driver.find_element_by_name("new_images[]").send_keys("D:\\cat.jpg")
+
+    tab=driver.find_element_by_css_selector("div.tabs")
+    tab.find_element_by_link_text("Information").click()
+    driver.implicitly_wait(10)
+    Select(driver.find_element_by_name("manufacturer_id")).select_by_value("1")
+    driver.find_element_by_name("short_description[en]").send_keys("Black funny Cat")
+
+    tab.find_element_by_link_text("Prices").click()
+    driver.implicitly_wait(10)
+    driver.find_element_by_name("purchase_price").send_keys(Keys.HOME + "1")
+    Select(driver.find_element_by_name("purchase_price_currency_code")).select_by_value("USD")
+
+    driver.find_element_by_name("save").click()
+    driver.implicitly_wait(10)
+
+    
+
